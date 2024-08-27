e3986e3ac560909990850131166f498396761f26
JorgeVinasco7 <jorgevinasco_77@live.com>
2024-08-27 12:05:51 -0500

    version_2

--- php/login.php
+++ php/login.php
@@ -1,3 +1,4 @@
+<?php
 require 'conexionDB.php';
 
 // Obtener datos del formulario del login
@@ -18,3 +19,4 @@ if ($result -> num_rows > 0) {
     // Si no se encontró el usuario
     echo "INVALIDO LOGIN O PASSWORD.";
 }
+?>
